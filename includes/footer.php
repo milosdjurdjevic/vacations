255541b85e1252dddbf79f1ac17dff6b54a2de33
milosdjurdjevic <milosdjurdjevicc@gmail.com>
2017-07-14 00:10:42 +0200

    minor fix

--- includes/footer.php
+++ includes/footer.php
@@ -3,11 +3,9 @@
     <p>&copy; Vacations 2017</p>
 </footer>
 </div> <!-- /container -->
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
 <script src="assets/js/vendor/bootstrap.min.js"></script>
 <script src="assets/js/datatables.min.js"></script>
-<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <script src="assets/js/main.js"></script>
 </body>
